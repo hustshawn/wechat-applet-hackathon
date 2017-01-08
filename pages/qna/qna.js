44ce9941d02d6f9f9522dfc4f50acf97eb8b45ae
Shawn Zhang <hustshawn@gmail.com>
2017-01-08 17:48:22 +0800

    QA

---
+++ pages/qna/qna.js
@@ -0,0 +1,19 @@
+//qna.js
+//获取应用实例
+Page({
+  data: {
+    msgs: [],
+    curmsg: ''
+  },
+  //事件处理函数
+  bindconfirm: function(e) {
+    console.log(e);
+    let newmsgs = [...this.data.msgs];
+    let newmsg = e.detail.value;
+    newmsgs.push(newmsg);
+    this.setData({
+      msgs: newmsgs,
+      curmsg: ''
+    });
+  }
+})
